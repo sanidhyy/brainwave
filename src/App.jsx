e3262b35e4b8c670b4e885bbe6af4f0a63a9211c
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-10 12:36:37 +0000

    css setup and reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import "./App.css";
-
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
 
 export default App;
