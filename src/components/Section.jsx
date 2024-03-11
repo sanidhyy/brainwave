b345a3f55a37b8482f484ba340771a63f7f73258
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-11 05:11:45 +0000

    section component implemented

---
+++ src/components/Section.jsx
@@ -0,0 +1,46 @@
+import SectionSvg from "../assets/svg/SectionSvg";
+
+const Section = ({
+  className,
+  id,
+  crosses,
+  crossesOffset,
+  customPaddings,
+  children,
+}) => {
+  return (
+    <div
+      id={id}
+      className={`relative ${
+        customPaddings ||
+        `py-10 lg:py-16 xl:py-20 ${crosses && "lg:py-32 xl:py-40"}`
+      } ${className || ""}`}
+    >
+      {children}
+
+      <div
+        className="hidden absolute top-0 left-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:left-7.5 xl:left-10"
+        aria-hidden
+      />
+      <div
+        className="hidden absolute top-0 left-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:right-7.5 xl:right-10"
+        aria-hidden
+      />
+
+      {crosses && (
+        <>
+          <div
+            className={`hidden absolute top-0 left-7.5 right-7.5 h-0.25 bg-stroke-1 ${
+              crossesOffset || ""
+            } pointer-events-none lg:block xl:left-10 right-10`}
+            aria-hidden
+          />
+
+          <SectionSvg crossesOffset={crossesOffset || ""} />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Section;
