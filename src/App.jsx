a939089c27e90c856052ab4ba1199c8779899e65
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-11 04:44:45 +0000

    desktop header completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import Button from "./components/Button";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Button className="mt-10">Something</Button>
+        <Header />
       </div>
       <ButtonGradient />
     </>
