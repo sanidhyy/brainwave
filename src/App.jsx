63465834ace3803eeb79f174c278683ef0dea4c3
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-12 05:24:31 +0000

    collaboration section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 
@@ -10,6 +11,7 @@ const App = () => {
         <Header />
         <Hero />
         <Benefits />
+        <Collaboration />
       </div>
       <ButtonGradient />
     </>
