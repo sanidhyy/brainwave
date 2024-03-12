991d6f2c083a6a8bd404371aa8b6a6704d870055
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-12 05:58:16 +0000

    text added

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,7 +1,8 @@
-const Heading = ({ className, title }) => {
+const Heading = ({ className, title, text }) => {
   return (
     <div className={`${className || ""} max-w-[50rem] mx-auto mb-12 lg:mb-20`}>
       {title && <h2 className="h2">{title}</h2>}
+      {text && <p className="body-2 mt-4 text-n-4">{text}</p>}
     </div>
   );
 };
