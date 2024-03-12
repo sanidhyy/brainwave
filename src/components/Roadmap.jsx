0f599e641e737ffcef2e31d1afc72179e5f80668
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-12 10:06:00 +0000

    typo fix

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -9,7 +9,7 @@ import { Gradient } from "./design/Roadmap";
 const Roadmap = () => (
   <Section className="overflow-hidden" id="roadmap">
     <div className="container md:pb-10">
-      <Heading tag="Ready to get started" title="What we’re working on" />
+      <Heading tag="Ready to get started" title="What we're working on" />
 
       <div className="relative grid gap-6 md:grid-cols-2 md:gap-4 md:pb-[7rem]">
         {roadmap.map((item) => {
