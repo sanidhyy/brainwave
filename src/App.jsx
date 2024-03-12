f5dd735c1af9af07cef0393e7c11f40696756367
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-12 10:11:27 +0000

    footer completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
@@ -18,6 +19,7 @@ const App = () => {
         <Services />
         <Pricing />
         <Roadmap />
+        <Footer />
       </div>
       <ButtonGradient />
     </>
---
+++ src/components/Footer.jsx
@@ -0,0 +1,35 @@
+import { socials } from "../constants";
+import Section from "./Section";
+
+const Footer = () => {
+  return (
+    <Section crosses className="!px-0 !py-10">
+      <div className="container flex sm:justify-between justify-center items-center gap-10 max-sm:flex-col">
+        <p className="caption text-n-4 lg:block">
+          &copy; Brainwave {new Date().getFullYear()}. All rights reserved.
+        </p>
+
+        <ul className="flex gap-5 flex-wrap">
+          {socials.map((social) => (
+            <a
+              href={social.url}
+              key={social.id}
+              target="_blank"
+              rel="noreferrer noopener"
+              className="flex items-center justify-center w-10 h-10 bg-n-7 rounded-full transition-colors hover:bg-n-6"
+            >
+              <img
+                src={social.iconUrl}
+                alt={social.title}
+                width={16}
+                height={16}
+              />
+            </a>
+          ))}
+        </ul>
+      </div>
+    </Section>
+  );
+};
+
+export default Footer;
