926e1f14e64ac6dd2ff92f9180b77b9633fe5835
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-12 05:58:07 +0000

    animation added

--- src/components/Generating.jsx
+++ src/components/Generating.jsx
@@ -7,8 +7,8 @@ const Generating = ({ className }) => {
         className || ""
       } text-base`}
     >
-      <img src={loading} alt="Loading" className="w-5 h-5 mr-4" />
-      AI is generating
+      <img src={loading} alt="Loading" className="w-5 h-5 mr-4 animate-spin" />
+      <span className="animate-pulse">AI is generating...</span>
     </div>
   );
 };
