7f3e29ce45feeefc9aa45b6f8d313c4033ee4dfc
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-12 10:05:33 +0000

    imports refactor

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -1,9 +1,9 @@
+import { check2, grid, loading1 } from "../assets";
+import { roadmap } from "../constants";
 import Button from "./Button";
 import Heading from "./Heading";
 import Section from "./Section";
 import Tagline from "./Tagline";
-import { roadmap } from "../constants";
-import { check2, grid, loading1 } from "../assets";
 import { Gradient } from "./design/Roadmap";
 
 const Roadmap = () => (
