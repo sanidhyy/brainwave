84f3ec538e3cecac8b38c6ae06ee89412b5c5a55
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-12 07:53:21 +0000

    pause svg icons added

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,6 +3,7 @@ import check from "./check.svg";
 import brainwaveSymbol from "./brainwave-symbol.svg";
 import brainwaveWhiteSymbol from "./brainwave-symbol-white.svg";
 import play from "./play.svg";
+import pause from "./pause.svg";
 import gradient from "./gradient.png";
 import smallSphere from "./4-small.png";
 import grid from "./grid.png";
@@ -83,6 +84,7 @@ export {
   brainwaveSymbol,
   brainwaveWhiteSymbol,
   play,
+  pause,
   gradient,
   smallSphere,
   grid,
