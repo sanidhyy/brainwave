77f8ae80d03f2d528beb546087dd57c55f76d96a
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-03-13 05:28:22 +0000

    fixed wrong spacing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -44,7 +44,7 @@ const Hero = () => {
 
           <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
             Unleast the power of AI within Brainwave. Upgrade your productivity
-            with
+            with{" "}
             <span className="inline-block relative font-semibold">
               Brainwave
               <img
