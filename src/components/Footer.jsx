70ad70d49e27dce3013c87e3e77720b64fc9b17d
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2025-03-24 08:51:31 +0530

    fix: footer formatting

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,9 @@ const Footer = () => {
     <Section crosses className="!px-0 !py-10">
       <div className="container flex sm:justify-between justify-center items-center gap-10 max-sm:flex-col">
         <p className="caption text-n-4 lg:block">
-          &copy; Brainwave {new Date().getFullYear()}. All rights reserved.
+          <span className="font-semibold font-serif text-xs">&copy;</span>{" "}
+          <span className="text-color-1 font-medium">Brainwave</span>{" "}
+          {new Date().getFullYear()}. All rights reserved.
         </p>
 
         <ul className="flex gap-5 flex-wrap">
